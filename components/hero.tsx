b4958263f0aedabc10c61590a5d6f06bda036004
Ro11ingButler <rollingkai1214@gmail.com>
2024-05-14 16:42:56 +0800

    fix contact button positioning

--- components/hero.tsx
+++ components/hero.tsx
@@ -18,9 +18,9 @@ export default async function Hero() {
         <h1 className="text-4xl font-bold tracking-widest">{res.name}</h1>
         <h2 className="text-xl font-medium tracking-wide">{res.selfTitle}</h2>
       </section>
-      <section className="fixed right-0 top-6 z-20 flex h-20 w-64 items-center justify-end">
+      <section className="fixed right-0 top-2 z-20 flex h-20 w-64 items-center justify-end">
         <a
-          className="absolute mr-8 rounded-xl border border-[#325c59] border-opacity-0 px-5 py-2.5 text-base font-medium tracking-widest underline transition-all duration-200 hover:border-opacity-100 hover:bg-[#ece8f5] hover:no-underline"
+          className="absolute mr-8 rounded-xl border border-[#325c59] border-opacity-0 px-5 py-2.5 text-sm font-medium tracking-widest underline transition-all duration-200 hover:border-opacity-100 hover:bg-[#ece8f5] hover:no-underline"
           href={`mailto:${res.email}`}
         >
           CONTACT
